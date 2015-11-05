b71a4b32ba93364dafafd178ddd48caeb7c53559
FFMG <github@myoddweb.com>
2015-11-05 21:16:52 +0200

    Updated assembly version

--- Classifier.Outlook/Properties/AssemblyInfo.cs
+++ Classifier.Outlook/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Myoddweb.Classifier")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Email classifier")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("http://www.myoddweb.com")]
 [assembly: AssemblyProduct("Myoddweb.Classifier")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
