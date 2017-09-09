d1258fa774d84e16abe3b6e7fc080c4ffbfe879a
FFMG <github@myoddweb.com>
2017-09-09 15:44:12 +0200

    Updated version number and fixed test

--- common/myoddweb.classifierUnitTest/TestGeneral.cs
+++ common/myoddweb.classifierUnitTest/TestGeneral.cs
@@ -27,14 +27,14 @@ namespace myoddweb.classifierUnitTest
     public void TestGetVersionNumber()
     {
       var versionNumber = TheEngine.GetEngineVersionNumber();
-      Assert.AreEqual(1006001, versionNumber );
+      Assert.AreEqual(1006002, versionNumber );
     }
 
     [TestMethod]
     public void TestGetVersion()
     {
       var version = TheEngine.GetEngineVersion();
-      Assert.AreEqual( new Version(1, 6, 1, 0), version);
+      Assert.AreEqual( new Version(1, 6, 2, 0), version);
     }
   }
 }
