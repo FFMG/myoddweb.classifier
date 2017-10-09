4c296fd40d1adc6661e57a80ae95b43095c3dd69
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2017-10-09 10:08:17 +0200

    More tests and more work for Engine > IEngine

---
+++ common/myoddweb.classifierUnitTest/TestUnProcessedFolders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using myoddweb.classifier.core;
+using Moq;
+using NUnit.Framework;
+
+namespace myoddweb.classifierUnitTest64
+{
+  [TestFixture]
+  class TestUnProcessedFolders
+  {
+    [Test]
+    public void TestConstructorEngineCannotBeNull()
+    {
+      var ns = new Mock<Microsoft.Office.Interop.Outlook._NameSpace>();
+      var en = new Mock<IEngine>();
+      var mp = new MailProcessor( en.Object, ns.Object );
+
+      Assert.Throws<ArgumentNullException>(() => new UnProcessedFolders( null, mp ));
+    }
+
+    [Test]
+    public void TestConstructorMailProcessorCannotBeNull()
+    {
+      var en = new Mock<IEngine>();
+
+      Assert.Throws<ArgumentNullException>(() => new UnProcessedFolders(en.Object, null ));
+    }
+  }
+}
