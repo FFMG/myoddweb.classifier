779255e367c6ae1887225c5dcab23a3eb8adcd8e
FFMG <github@myoddweb.com>
2017-10-14 11:00:22 +0200

    ILogger is now a variable of IEngine rather than derived.

---
+++ Classifier.Outlook/core/logger.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Classifier.Interfaces.Helpers;
+using myoddweb.classifier.interfaces;
+using Classifier.Interfaces;
+using Newtonsoft.Json;
+using myoddweb.classifier.utils;
+
+namespace myoddweb.classifier.core
+{
+  public class Logger : ILogger
+  {
+    /// <summary>
+    /// The classification engine.
+    /// </summary>
+    private readonly IClassify1 ClassifyEngine;
+
+    private readonly Options Options;
+    public Logger(IClassify1 classifyEngine, Options options )
+    {
+      ClassifyEngine = classifyEngine;
+      Options = options;
+    }
+
+    /// <summary>
+    /// Get up to 'max' log entries.
+    /// </summary>
+    /// <param name="max">The max number of log entries we want to get.</param>
+    /// <returns></returns>
+    public List<LogEntry> GetLogEntries(int max)
+    {
+      // get the log entries,
+      List<LogEntry> entries;
+      return -1 == ClassifyEngine.GetLogEntries(out entries, max) ? null : entries;
+    }
+
+    /// <summary>
+    /// Log an error message
+    /// </summary>
+    /// <param name="message"></param>
+    public virtual void LogError(string message)
+    {
+      // log it
+      LogMessageToEngine(message, Options.LogLevels.Error);
+    }
+
+    /// <summary>
+    /// Log a warning message
+    /// </summary>
+    /// <param name="message"></param>
+    public virtual void LogWarning(string message)
+    {
+      // log it
+      LogMessageToEngine(message, Options.LogLevels.Warning);
+    }
+
+    /// <summary>
+    /// Log an information message
+    /// </summary>
+    /// <param name="message"></param>
+    public virtual void LogInformation(string message)
+    {
+      // log it
+      LogMessageToEngine(message, Options.LogLevels.Information);
+    }
+
+    /// <summary>
+    /// Log a verbose message
+    /// </summary>
+    /// <param name="message"></param>
+    public virtual void LogVerbose(string message)
+    {
+      // log it
+      LogMessageToEngine(message, Options.LogLevels.Verbose);
+    }
+
+
+    /// <summary>
+    /// Log a message to the engine
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="level"></param>
+    private void LogMessageToEngine(string message, Options.LogLevels level)
+    {
+      // can we log this?
+      if (!Options.CanLog(level))
+      {
+        return;
+      }
+
+      //  create the json string.
+      var lm = new LogData { Level = level, Message = message };
+      string json = JsonConvert.SerializeObject(lm, Formatting.None);
+
+      // log the string now.
+      ClassifyEngine.Log(LogSource(level), json);
+    }
+
+    private static string LogSource(Options.LogLevels level)
+    {
+      return $"{System.Diagnostics.Process.GetCurrentProcess().ProcessName}.{level}";
+    }
+  }
+}
---
+++ Classifier.Outlook/core/outlooklogger.cs
@@ -0,0 +1,116 @@
+﻿using Classifier.Interfaces;
+
+namespace myoddweb.classifier.core
+{
+  public class OutlookLogger : Logger
+  {
+    /// <summary>
+    /// Name for logging in the event viewer,
+    /// </summary>
+    private readonly string EventViewSource;
+
+    /// <summary>
+    /// If NULL we have not check for event source.
+    /// Any other value, we will check for.
+    /// </summary>
+    private bool? _eventSource = null;
+
+    public OutlookLogger( string eventSource, IClassify1 classifyEngine, Options options) : 
+      base(classifyEngine, options)
+    {
+      EventViewSource = eventSource;
+    }
+
+    private bool InstallAndValidateSource()
+    {
+      if (null != _eventSource)
+      {
+        return (bool)_eventSource;
+      }
+
+      try
+      {
+        if (!System.Diagnostics.EventLog.SourceExists(EventViewSource))
+        {
+          System.Diagnostics.EventLog.CreateEventSource(EventViewSource, null);
+        }
+
+        // set the value.
+        _eventSource = System.Diagnostics.EventLog.SourceExists(EventViewSource);
+      }
+      catch (System.Security.SecurityException)
+      {
+        _eventSource = false;
+      }
+
+      // one last check.
+      return InstallAndValidateSource();
+    }
+
+    /// <summary>
+    /// Log an error message
+    /// </summary>
+    /// <param name="message"></param>
+    public override void LogError(string message)
+    {
+      // log to the event log.
+      if (!InstallAndValidateSource())
+      {
+        return;
+      }
+
+      var appLog = new System.Diagnostics.EventLog { Source = EventViewSource };
+      appLog.WriteEntry(message, System.Diagnostics.EventLogEntryType.Error);
+    }
+
+    /// <summary>
+    /// Log a warning message
+    /// </summary>
+    /// <param name="message"></param>
+    public override void LogWarning(string message)
+    {
+      // log to the event log.
+      if (!InstallAndValidateSource())
+      {
+        return;
+      }
+
+      var appLog = new System.Diagnostics.EventLog { Source = EventViewSource };
+      appLog.WriteEntry(message, System.Diagnostics.EventLogEntryType.Warning);
+    }
+
+    /// <summary>
+    /// Log an information message
+    /// </summary>
+    /// <param name="message"></param>
+    public override void LogInformation(string message)
+    {
+      // log to the event log.
+      if (!InstallAndValidateSource())
+      {
+        return;
+      }
+
+      var appLog = new System.Diagnostics.EventLog { Source = EventViewSource };
+      appLog.WriteEntry(message, System.Diagnostics.EventLogEntryType.Information);
+    }
+
+    /// <summary>
+    /// Log a verbose message
+    /// </summary>
+    /// <param name="message"></param>
+    public override void LogVerbose(string message)
+    {
+      base.LogVerbose(message);
+
+      // log to the event log.
+      if (!InstallAndValidateSource())
+      {
+        return;
+      }
+
+      var appLog = new System.Diagnostics.EventLog { Source = EventViewSource };
+      appLog.WriteEntry(message, System.Diagnostics.EventLogEntryType.Information);
+    }
+  }
+}
